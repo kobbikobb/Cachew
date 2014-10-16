92e3e746feeb9a2de08396719ad7fdaf58acb930
Jakob <jakob@motus.is>
2014-10-16 14:10:51 +0000

    CacheKey introduced

---
+++ Cachew.Tests/CacheKeyTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Cachew.Tests
+{
+    [TestFixture]
+    public class CacheKeyTests
+    {
+        [Test]
+        [TestCase("GetObjects")]
+        [TestCase("GetBar")]
+        [TestCase("GetBoo")]
+        public void TwoKeysWithSameNameAndNoParametersAreEqual(string name)
+        {
+            var key1 = new CacheKey(name);
+            var key2 = new CacheKey(name);
+
+            Assert.AreEqual(key1, key2);
+        }
+
+        [Test]
+        [TestCase("GetObjects", "GetObjectS")]
+        [TestCase("GetBar", "GetFoo")]
+        [TestCase("GetBoo", "")]
+        public void TwoKeysWithDifferentNameAndNoParametersAreNotEqual(string name1, string name2)
+        {
+            var key1 = new CacheKey(name1);
+            var key2 = new CacheKey(name2);
+
+            Assert.AreNotEqual(key1, key2);
+        }
+
+        [Test]
+        [TestCase("GetObjects", 3, "argument")]
+        [TestCase("GetBar", 45, "king")]
+        [TestCase("GetBoo", 65, "pirate")]
+        public void TwoKeysWithSameNameAndSameParametersAreEqual(string name, int argument1, string argument2)
+        {
+            var key1 = new CacheKey(name, argument1, argument2);
+            var key2 = new CacheKey(name, argument1, argument2);
+
+            Assert.AreEqual(key1, key2);
+        }
+
+        [Test]
+        [TestCase("GetObjects", 3, 4, "argument", "argument")]
+        [TestCase("GetObjects", 3, 3, "argument", "argumenT")]
+        [TestCase("GetBar", 45, 4, "king", "king")]
+        [TestCase("GetBar", 45, 45, "king", "spider")]
+        public void TwoKeysWithSameNameAndDifferentParametersAreNotEqual(
+            string name, int intArgument1, int intArgument2, string stringArgument1, string stringArgument2)
+        {
+            var key1 = new CacheKey(name, intArgument1, stringArgument1);
+            var key2 = new CacheKey(name, intArgument2, stringArgument2);
+
+            Assert.AreNotEqual(key1, key2);
+        }
+
+        [Test]
+        public void KeyNameCanNotBeNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new CacheKey(null));
+        }
+    }
+}
---
+++ Cachew/CacheKey.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace Cachew
+{
+    public class CacheKey : IEquatable<CacheKey>
+    {
+        private readonly string name;
+        private readonly object[] arguments;
+
+        public CacheKey(string name, params object[] arguments)
+        {
+            if (name == null) throw new ArgumentNullException("name");
+            this.name = name;
+            this.arguments = arguments;
+        }
+
+        #region Equals
+
+        public bool Equals(CacheKey other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+
+            if (!string.Equals(name, other.name))
+                return false;
+
+            if (arguments == null && other.arguments == null)
+                return true;
+            
+            if (arguments == null || other.arguments == null)
+                return false;
+
+            return arguments.SequenceEqual(other.arguments);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((CacheKey)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((arguments != null ? arguments.GetHashCode() : 0) * 397) ^ (name != null ? name.GetHashCode() : 0);
+            }
+        }
+
+        #endregion
+    }
+}
